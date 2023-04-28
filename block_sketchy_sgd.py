5db66f8df7f4835cf701fb78e5014f376caa30a1
Ashboy64 <ashish.arartc@gmail.com>
2023-04-27 17:09:03 -0700

    Form sketch + start on nystrom

--- block_sketchy_sgd.py
+++ block_sketchy_sgd.py
@@ -17,42 +17,85 @@ class BlockSketchySGD:
         for p in self.model.parameters():
             if p.grad is not None:
                 p.grad.data.zero_()
-
-    def _hvp(self, param, grad, vec):
-
-        print(grad)
-        print(param)
-        print(param.grad)
-
-        out = torch.autograd.grad(
-            grad.reshape(-1,), param.reshape(-1,), 
-            grad_outputs=vec, only_inputs=True, 
-            allow_unused=True
-        )
-        print(out)
-        return out 
     
-    def _make_sketch(self, param, grad):
-        vecs = torch.randn(self.block_rank, torch.numel(param))
-        hvps = []
-        for i in range(self.block_rank):
-            hvps.append(self._hvp(param, grad, vecs[i]))
-        return torch.stack(hvps)
+    def rand_nys_approx(self, Y, Q):    # Y is sketch, Q is test matrix (vecs of HVP)
+        # Naive implementation
+        # return Y @ torch.linalg.solve(Q.T @ Y, Y.T)
+
+        # Sketchy SGD style
+        p = Y.shape[0] 
+        eps = 1e-6      # TODO: Set according to actual algo
+        nu = (p**0.5) * eps
+        
+        Y_nu = Y + nu*Q 
+        print("Computed Y_nu")
+        
+        # This is where the program hangs
+        C, _ = torch.linalg.cholesky_ex(Q.T @ Y_nu)
+        print("Computed C")
+        
+        B = torch.linalg.solve_triangular(C, Y)
+        print("Computed B")
+        
+        V_hat, Sigma, _ = torch.linalg.svd(B, full_matrices=False)
+        print("Computed V_hat, Sigma")
+        
+        Lam_hat = torch.maximum(Sigma@Sigma - nu, 0)
+        print("Computed Lam_hat")
+        
+        return V_hat, Lam_hat
+
+
     
     def step(self, loss_tensor, model):
-        for p in self.model.parameters():
-            # g is the gradient of the loss wrt p
-            g = torch.autograd.grad(
-                loss_tensor, p, create_graph=True
-            )
+        # Compute gradients
+        gs = torch.autograd.grad(
+            loss_tensor, model.parameters(), create_graph=True, 
+            retain_graph=True
+        )
 
-            # Vector with which we are computing HVP
-            v = torch.randn(self.block_rank, torch.numel(p))
+        for g, p in zip(gs, self.model.parameters()):
+            # Form sketch
+            vs = torch.randn(torch.numel(p), self.block_rank)  # HVP vectors
+            vs = torch.linalg.qr(vs, 'reduced').Q.T    # Reduced / Thin QR
 
-            hvp = torch.autograd.grad(
-                g[0].reshape(-1,), p.reshape(-1,), 
-                grad_outputs=torch.ones(torch.numel(p)), only_inputs=True, 
-                allow_unused=True
-            )
+            sketch = []
+            for i in range(self.block_rank):
+                v = vs[i].reshape(*p.shape)
+                hvp = torch.autograd.grad(
+                    g, p, 
+                    grad_outputs=v,
+                    only_inputs=True, allow_unused=True, 
+                    retain_graph=True
+                )
+                sketch.append(hvp[0].reshape(-1,))
+            sketch = torch.stack(sketch)
 
-            print(hvp)      # Why is this None???
+            # TODO: This freezes
+            V_hat, Lam_hat = self.rand_nys_approx(sketch, vs)
+    
+
+    # OLD CODE
+    # For some reason using a function like this does not work; maybe something is 
+    # being copied that is causing the hvp to become none?
+    # def _hvp(self, grad, param, vec):
+
+    #     hvp = torch.autograd.grad(
+    #         grad, param, 
+    #         grad_outputs=vec,
+    #         only_inputs=True, allow_unused=True, 
+    #         retain_graph=True
+    #     )
+
+    #     return hvp
+    
+    # def _make_sketch(self, param, grad):
+    #     vecs = torch.randn(self.block_rank, torch.numel(param))
+    #     hvps = []
+    #     for i in range(self.block_rank):
+    #         hvps.append(self._hvp(param, grad, vecs[i]))
+    #     return torch.stack(hvps)
+
+
+if __name__ == '__main__':
+    hvp_test()
--- test_optimizer.py
+++ test_optimizer.py
@@ -18,7 +18,7 @@ LR = 3e-4
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def main():
+def main():    
     train_loader, val_loader, test_loader = load_data(BATCH_SIZE)
     
     model = MLP(28*28, 10)
@@ -40,5 +40,35 @@ def main():
             break
 
 
+# This is a test to figure out what is going on in the method to compute HVPs that does 
+# not return null. It seems like based on the output it is actually computing H.T @ v 
+# instead of H @ v.
+# EDIT: It is computing H.T, verified from 
+# https://pytorch.org/tutorials/beginner/blitz/autograd_tutorial.html
+def hvp_test():
+    p = torch.tensor([ [1., 1.], [0., 1.] ], requires_grad=True)
+    A = torch.tensor([ [2., 4.], [3., 2.] ], requires_grad=True)
+    g = A @ p 
+
+    J = torch.tensor([ [2, 0, 4, 0], [0, 2, 0, 4], [3, 0, 2, 0], [0, 3, 0, 2] ], dtype=torch.float32, requires_grad=False).T
+
+    # v = torch.randn_like(p)
+    # v = torch.ones_like(p)
+    v = torch.tensor([[1., 0.], [0., 0.]])
+    y = J @ v.reshape(-1,)
+
+    jvp = torch.autograd.grad(
+        # g.view(-1), p.view(-1), 
+        g, p,
+        grad_outputs=v,
+        only_inputs=True, allow_unused=True, 
+        retain_graph=True
+    )
+
+    print(f"{y}")
+    print(f"{jvp}")
+
+
 if __name__ == '__main__':
+    # hvp_test()
     main()
