ee512922602659607d72696a325d45f598c8b88e
Ashboy64 <ashish.arartc@gmail.com>
2023-04-29 08:17:24 -0700

    bug fixes + computing step via woodbury

--- block_sketchy_sgd.py
+++ block_sketchy_sgd.py
@@ -8,45 +8,41 @@ import torch
 
 class BlockSketchySGD:
     
-    def __init__(self, model, block_rank=3, lr=3e-4):
+    def __init__(self, model, lr=3e-4, block_rank=3, rho=1e-3):
         self.lr = lr 
         self.model = model
         self.block_rank = block_rank
+        self.rho = rho      # Constant for Woodbury inverse
     
+
     def zero_grad(self):
         for p in self.model.parameters():
             if p.grad is not None:
                 p.grad.data.zero_()
     
-    def rand_nys_approx(self, Y, Q):    # Y is sketch, Q is test matrix (vecs of HVP)
-        # Naive implementation
-        # return Y @ torch.linalg.solve(Q.T @ Y, Y.T)
 
-        # Sketchy SGD style
+    def rand_nys_approx(self, Y, Q):    # Y is sketch, Q is test matrix (vecs of HVP)
         p = Y.shape[0] 
         eps = 1e-6      # TODO: Set according to actual algo
         nu = (p**0.5) * eps
         
         Y_nu = Y + nu*Q 
-        print("Computed Y_nu")
-        
-        # This is where the program hangs
         C, _ = torch.linalg.cholesky_ex(Q.T @ Y_nu)
-        print("Computed C")
+        B = torch.linalg.solve_triangular(C, Y_nu.T, upper=False)
+        U, Sigma, _ = torch.linalg.svd(B.T, full_matrices=False)
+        Lam_hat = torch.clip(Sigma**2 - nu, min=0)
         
-        B = torch.linalg.solve_triangular(C, Y)
-        print("Computed B")
-        
-        V_hat, Sigma, _ = torch.linalg.svd(B, full_matrices=False)
-        print("Computed V_hat, Sigma")
-        
-        Lam_hat = torch.maximum(Sigma@Sigma - nu, 0)
-        print("Computed Lam_hat")
-        
-        return V_hat, Lam_hat
-
-
+        return U, Lam_hat
     
+    
+    def approx_newton_step(self, V_hat, Lam_hat, g):
+        g = g.reshape(-1,)
+        first_term = V_hat @ (torch.diag(1 / (Lam_hat + self.rho)) @ (V_hat.T @ g))
+        second_term = (g - V_hat@(V_hat.T@g)) / self.rho
+
+        return first_term + second_term
+    
+
     def step(self, loss_tensor, model):
         # Compute gradients
         gs = torch.autograd.grad(
@@ -57,11 +53,11 @@ class BlockSketchySGD:
         for g, p in zip(gs, self.model.parameters()):
             # Form sketch
             vs = torch.randn(torch.numel(p), self.block_rank)  # HVP vectors
-            vs = torch.linalg.qr(vs, 'reduced').Q.T    # Reduced / Thin QR
+            vs = torch.linalg.qr(vs, 'reduced').Q    # Reduced / Thin QR
 
             sketch = []
             for i in range(self.block_rank):
-                v = vs[i].reshape(*p.shape)
+                v = vs[:, i].reshape(*p.shape)
                 hvp = torch.autograd.grad(
                     g, p, 
                     grad_outputs=v,
@@ -69,33 +65,34 @@ class BlockSketchySGD:
                     retain_graph=True
                 )
                 sketch.append(hvp[0].reshape(-1,))
-            sketch = torch.stack(sketch)
+            sketch = torch.stack(sketch).T
 
-            # TODO: This freezes
+            # Approx eigendecomposition of Hessian via randomized nystrom method
             V_hat, Lam_hat = self.rand_nys_approx(sketch, vs)
-    
 
-    # OLD CODE
-    # For some reason using a function like this does not work; maybe something is 
-    # being copied that is causing the hvp to become none?
-    # def _hvp(self, grad, param, vec):
-
-    #     hvp = torch.autograd.grad(
-    #         grad, param, 
-    #         grad_outputs=vec,
-    #         only_inputs=True, allow_unused=True, 
-    #         retain_graph=True
-    #     )
-
-    #     return hvp
-    
-    # def _make_sketch(self, param, grad):
-    #     vecs = torch.randn(self.block_rank, torch.numel(param))
-    #     hvps = []
-    #     for i in range(self.block_rank):
-    #         hvps.append(self._hvp(param, grad, vecs[i]))
-    #     return torch.stack(hvps)
+            # Invert using Woodbury formula and get approx Newton step
+            step = self.approx_newton_step(V_hat, Lam_hat, g).reshape(*p.shape)
+            p.data.add_(self.lr * step)
 
 
-if __name__ == '__main__':
-    hvp_test()
+
+# OLD CODE
+# For some reason using a function like this does not work; maybe something is 
+# being copied that is causing the hvp to become none?
+# def _hvp(self, grad, param, vec):
+
+#     hvp = torch.autograd.grad(
+#         grad, param, 
+#         grad_outputs=vec,
+#         only_inputs=True, allow_unused=True, 
+#         retain_graph=True
+#     )
+
+#     return hvp
+
+# def _make_sketch(self, param, grad):
+#     vecs = torch.randn(self.block_rank, torch.numel(param))
+#     hvps = []
+#     for i in range(self.block_rank):
+#         hvps.append(self._hvp(param, grad, vecs[i]))
+#     return torch.stack(hvps)
