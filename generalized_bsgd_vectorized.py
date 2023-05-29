6f93a2f3dcb84e40504001836e0b653a792d6ca2
Ashboy64 <ashish.arartc@gmail.com>
2023-05-29 16:43:27 -0700

    finish vectorized implementation

--- generalized_bsgd_vectorized.py
+++ generalized_bsgd_vectorized.py
@@ -4,6 +4,7 @@
 # - https://mcneela.github.io/machine_learning/2019/09/03/Writing-Your-Own-Optimizers-In-Pytorch.html
 
 
+import math
 import numpy as np
 import torch
 from filters import * 
@@ -29,24 +30,31 @@ class GeneralizedBSGDVectorized:
         self.h_recomp_interval = h_recomp_interval
 
         self.sketches = [None for p in model.parameters()]
-        self.cache = [[None for i in range(torch.numel(p) // self.block_size)] \
-            for p in model.parameters()]
 
         self.p_idx_to_block_info = {}
+        self.p_idx_to_uneven_block = {}
+
         for p_idx, p in enumerate(model.parameters()):
             block_info = []
+            uneven_block_info = None
 
             p_size = torch.numel(p)
-            for i in range(p_size // self.block_size):
+            for i in range(math.ceil(p_size / self.block_size)):
                 block_start = self.block_size * i 
-                block_end = min(self.block_size * (i + 1), p_size)
-                block_info.append( list(range(block_start, block_end)) )
+
+                block_end = self.block_size * (i + 1)
+                if block_end <= p_size:
+                    block_info.append( list(range(block_start, block_end)) )
+                else:
+                    uneven_block_info = list(range(block_start, p_size))
+                    uneven_block_info = torch.as_tensor(uneven_block_info, dtype=torch.long)
+                    # (index info, inv hessian block)
+                    self.p_idx_to_uneven_block[p_idx] = (uneven_block_info, None)
             
             block_info = torch.as_tensor(block_info, dtype=torch.long)
             self.p_idx_to_block_info[p_idx] = block_info
         
         self.inv_block_hessians = [None for p in model.parameters()]
-
 
 
     def zero_grad(self):
@@ -55,12 +63,18 @@ class GeneralizedBSGDVectorized:
                 p.grad.data.zero_()
     
 
-    def compute_inv_block_hess(self, block_idx_info, p_idx):
-        mid, sketch = self.sketches[p_idx]
+    def compute_inv_block_hess(self, block_idx_info, mid, sketch):
         subsketch = torch.take_along_dim(sketch, block_idx_info.reshape(-1, 1), dim=0)
         block_hess_approx = subsketch @ mid @ subsketch.T 
         inv_block_hess = torch.linalg.pinv(block_hess_approx, atol=1e-6)
         return inv_block_hess
+    
+    def compute_newton_step(self, block_idx_info, inv_block_hess, g_flat):
+        if len(block_idx_info.shape) == 0:
+            return torch.Tensor()
+        block_step = -inv_block_hess @ torch.take_along_dim(g_flat, block_idx_info, dim=0)
+        block_step /= torch.clip(torch.linalg.norm(block_step), min=1.)
+        return block_step
 
 
     @torch.no_grad()
@@ -93,23 +107,29 @@ class GeneralizedBSGDVectorized:
                 
                 vs = torch.randn(torch.numel(p), num_hvps)  # HVP vectors
                 vs = torch.linalg.qr(vs, 'reduced').Q    # Reduced / Thin QR
-                sketch = torch.vmap(get_hvp)(vs.T.reshape(num_hvps, *p.shape))
+                sketch_T = torch.vmap(get_hvp)(vs.T.reshape(num_hvps, *p.shape))
 
-                mid = torch.linalg.pinv(sketch @ vs, atol=1e-6)
-                self.sketches[p_idx] = (mid, sketch.T)
+                mid = torch.linalg.pinv(sketch_T @ vs, atol=1e-6)
             
                 # Recompute pseudoinverse of block if needed
-                self.inv_block_hessians[p_idx] = torch.vmap(lambda x: self.compute_inv_block_hess(x, p_idx=p_idx))(self.p_idx_to_block_info[p_idx])
+                inv_block_hess_fn = lambda x: self.compute_inv_block_hess(x, mid=mid, sketch=sketch_T.T)
+                self.inv_block_hessians[p_idx] = torch.vmap(inv_block_hess_fn)(self.p_idx_to_block_info[p_idx])
+                if p_idx in self.p_idx_to_uneven_block:
+                    uneven_idx_info, _ = self.p_idx_to_uneven_block[p_idx]
+                    uneven_inv_block_hess = self.compute_inv_block_hess(uneven_idx_info, mid=mid, sketch=sketch_T.T)
+                    self.p_idx_to_uneven_block[p_idx] = (uneven_idx_info, uneven_inv_block_hess)
 
             # Form approx Newton steps
-            def compute_newton_steps(inv_block_hess, block_idx_info):
-                block_step = -inv_block_hess @ torch.take_along_dim(g_flat, block_idx_info, dim=0)
-                block_step /= torch.clip(torch.linalg.norm(block_step), min=1.)
-                return block_step
+            newton_step_fn = lambda idx_info, inv_block_hess : self.compute_newton_step(idx_info, inv_block_hess, g_flat=g_flat)
+            p_step = torch.vmap(newton_step_fn)(self.p_idx_to_block_info[p_idx], self.inv_block_hessians[p_idx]).reshape(-1,)
 
-            p_step = torch.vmap(compute_newton_steps)(self.inv_block_hessians[p_idx], self.p_idx_to_block_info[p_idx])
+            if p_idx in self.p_idx_to_uneven_block:
+                block_step = self.compute_newton_step(*self.p_idx_to_uneven_block[p_idx], g_flat=g_flat)
+                p_step = torch.concat([p_step, block_step])
+            
             p_step = p_step.reshape(*p.shape)
             
+            # Filter and update
             filtered_step = self.filterer.step(g, p_step, None, None, p_idx).reshape(*p.shape)
             p.data.add_(self.curr_lrs[p_idx] * filtered_step)
             step_mags.append( torch.linalg.norm(self.curr_lrs[p_idx] * filtered_step) )
